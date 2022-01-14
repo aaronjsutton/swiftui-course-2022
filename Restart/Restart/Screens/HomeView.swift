ed8f6817609b67cd9fd2875f1cd96c0a095e42ee
Aaron Sutton <hey@aaron.as>
2022-01-14 18:26:03 -0500

    Update View components and home view

--- Restart/Restart/Screens/HomeView.swift
+++ Restart/Restart/Screens/HomeView.swift
@@ -13,13 +13,38 @@ struct HomeView: View {
   
     var body: some View {
       VStack(spacing: 20) {
-        Text("Home").font(.largeTitle)
+        Spacer()
         
+        ZStack {
+          CircleGroupView(ShapeColor: .gray, ShapeOpacity: 0.2)
+          
+          Image("character-2")
+            .resizable()
+            .scaledToFit()
+          .padding()
+        }
+        
+        Text("The time that leads to mastery is depdendent on the intensity of our focus")
+          .font(.title3)
+          .fontWeight(.light)
+          .foregroundColor(.secondary)
+          .multilineTextAlignment(.center)
+          .padding()
+        
+        Spacer()
+    
         Button(action: {
           isOnboardinViewActive = true
         }) {
-            Text("Restart")
+          Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+            .imageScale(.large)
+          Text("Restart")
+            .font(.system(.title3, design: .rounded))
+            .fontWeight(.bold)
         }
+          .buttonStyle(.borderedProminent)
+          .buttonBorderShape(.capsule)
+          .controlSize(.large)
       }
     }
 }
