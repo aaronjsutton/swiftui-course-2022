b3689808d458afc59f9d00af84ed85dad2a76442
Aaron Sutton <hey@aaron.as>
2022-01-19 02:34:00 -0500

    Add page view

---
+++ Fruits/Fruits/App/OnboardingView.swift
@@ -0,0 +1,26 @@
+//
+//  OnboardingView.swift
+//  Fruits
+//
+//  Created by Aaron Sutton on 1/19/22.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+      TabView {
+        ForEach(0..<5) { item in 
+          FruitCardView()
+        }
+      }
+      .tabViewStyle(PageTabViewStyle())
+      .padding(.vertical, 20)
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
---
+++ Fruits/Fruits/View/StartButtonView.swift
@@ -0,0 +1,38 @@
+//
+//  StartButtonView.swift
+//  Fruits
+//
+//  Created by Aaron Sutton on 1/19/22.
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    var body: some View {
+      Button(action: {
+          print("Exit the onboarding")
+      }) {
+        HStack(spacing: 8) {
+          Text("Start")
+          
+          Image(systemName: "arrow.right.circle")
+            .imageScale(.large)
+          
+        }
+        .padding(.horizontal, 16)
+        .padding(.vertical, 10)
+        .background(
+          Capsule().strokeBorder(.white, lineWidth: 1.25)
+        )
+      }
+      .accentColor(.white)
+    }
+}
+
+struct StartButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+      StartButtonView()
+        .preferredColorScheme(.dark)
+        .previewLayout(.sizeThatFits)
+    }
+}
