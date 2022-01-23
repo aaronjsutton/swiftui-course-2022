c931788b0ca29b73fba139b2e4cac1d461aa6f8a
Aaron Sutton <hey@aaron.as>
2022-01-23 16:24:16 -0500

    Map annotations part 2

--- Africa/Africa/App/MapView.swift
+++ Africa/Africa/App/MapView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
-		
+	
 	@State var region: MKCoordinateRegion = {
 		var mapCoordinates = CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377589)
 		var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
@@ -21,14 +21,53 @@ struct MapView: View {
 	var body: some View {
 		Map(coordinateRegion: $region, annotationItems: locations) {
 			item in
-			// MapMarker(coordinate: item.location, tint: .accentColor)
 			MapAnnotation(coordinate: item.location) {
-				Image("logo")
-					.resizable()
-					.scaledToFit()
-					.frame(width: 32, height: 32, alignment: .center)
+				MapAnnotationView(location: item)
 			}
 		}
+		.overlay(
+			HStack(alignment: .center, spacing: 12) {
+				Image("compass")
+					.resizable()
+					.scaledToFit()
+					.frame(width: 40, height: 40, alignment: .center)
+				
+				VStack(alignment: .leading, spacing: 3) {
+					HStack {
+						Text("Latitude: ")
+							.font(.footnote)
+							.fontWeight(.bold)
+							.foregroundColor(.accentColor)
+						Spacer()
+						Text("\(region.center.latitude)")
+							.font(.footnote)
+							.foregroundColor(.white)
+					}
+					
+					Divider()
+					
+					HStack {
+						Text("Longitude: ")
+							.font(.footnote)
+							.fontWeight(.bold)
+							.foregroundColor(.accentColor)
+						Spacer()
+						Text("\(region.center.longitude)")
+							.font(.footnote)
+							.foregroundColor(.white)
+					}
+				}
+			}
+				.padding(.vertical, 12)
+				.padding(.horizontal, 16)
+				.background(
+					Color.black
+						.cornerRadius(8)
+						.opacity(0.6)
+				)
+				.padding()
+			, alignment: .top
+		)
 	}
 }
 
