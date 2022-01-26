9174f0b07ea9a4fcd9623a7274e4b506dd1c9824
Aaron Sutton <hey@aaron.as>
2022-01-26 00:52:39 -0500

    Create project

---
+++ Devote/Devote/DevoteApp.swift
@@ -0,0 +1,20 @@
+//
+//  DevoteApp.swift
+//  Devote
+//
+//  Created by Aaron Sutton on 1/26/22.
+//
+
+import SwiftUI
+
+@main
+struct DevoteApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
