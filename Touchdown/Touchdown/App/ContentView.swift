559f7f3f36eb24c4240dde348b74b88c3d4cd672
Aaron Sutton <hey@aaron.as>
2022-01-25 19:04:15 -0500

    Nav bar

--- Touchdown/Touchdown/App/ContentView.swift
+++ Touchdown/Touchdown/App/ContentView.swift
@@ -8,15 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
-  
-  var body: some View {
-    FooterView()
-      .padding(.horizontal)
-  }
+	
+	var body: some View {
+		ZStack {
+			VStack(spacing: 0) {
+				
+				NavigationBarView()
+					.padding(.horizontal, 15)
+					.padding(.bottom)
+					.padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
+					.background(.white)
+					.shadow(color: .black.opacity(0.05), radius: 5, x: 0, y: 5)
+				
+				Spacer()
+				
+				FooterView()
+					.padding(.horizontal)
+			}
+			.background(colorBackground.ignoresSafeArea(.all, edges: .all))
+		}
+		.ignoresSafeArea(.all, edges: .top)
+	}
+	
 }
 
 struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView()
-  }
+	static var previews: some View {
+		ContentView()
+			.previewDevice("iPhone 13 Pro")
+	}
 }
